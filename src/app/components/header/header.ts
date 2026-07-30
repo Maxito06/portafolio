3dfb6c78da6d972f30d4cb088a1c27b82bc0cc83
Maxito06 <maxicopi5@gmail.com>
2026-07-30 14:38:38 -0500

    Agrega menú móvil tipo overlay al Header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  menuAbierto = signal(false);
+
+  toggleMenu() {
+    this.menuAbierto.update((v) => !v);
+  }
+
+  cerrarMenu() {
+    this.menuAbierto.set(false);
+  }
+}
